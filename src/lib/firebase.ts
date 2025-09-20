7f143ae46f362d0d5b386deb1ec8a46b059d8f48
neeevaa codes <neeevaacodes@gmail.com>
2025-09-20 18:22:31 +0000

    changed api key again

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
-import { getAuth, type Auth } from 'firebase/auth';
-
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBauvOVHec37k2Lw3NuM8KF1GHPx1hKC60",
-    authDomain: "studio-2631480391-e838b.firebaseapp.com",
-    projectId: "studio-2631480391-e838b",
-    storageBucket: "studio-2631480391-e838b.firebasestorage.app",
-    messagingSenderId: "984818009071",
-    appId: "1:984818009071:web:f30c0731ba56ad72fd6c1a"
-  };
+  apiKey: "AIzaSyBauvOVHec37k2Lw3NuM8KF1GHPx1hKC60",
+  authDomain: "studio-2631480391-e838b.firebaseapp.com",
+  projectId: "studio-2631480391-e838b",
+  storageBucket: "studio-2631480391-e838b.firebasestorage.app",
+  messagingSenderId: "984818009071",
+  appId: "1:984818009071:web:f30c0731ba56ad72fd6c1a"
+};
 
-// Initialize Firebase for client-side
-const app: FirebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const auth: Auth = getAuth(app);
-
-export { app, auth };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
