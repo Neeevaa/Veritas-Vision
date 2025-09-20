4f4e3002527995a3385eb18f4889d3190add0c26
neeevaa codes <neeevaacodes@gmail.com>
2025-09-20 18:26:05 +0000

    userauth set

--- src/hooks/use-auth.tsx
+++ src/hooks/use-auth.tsx
@@ -39,12 +39,12 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
   useEffect(() => {
     const firebaseConfig = {
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+      apiKey: "AIzaSyBauvOVHec37k2Lw3NuM8KF1GHPx1hKC60",
+  authDomain: "studio-2631480391-e838b.firebaseapp.com",
+  projectId: "studio-2631480391-e838b",
+  storageBucket: "studio-2631480391-e838b.firebasestorage.app",
+  messagingSenderId: "984818009071",
+  appId: "1:984818009071:web:f30c0731ba56ad72fd6c1a"
     };
 
     const app: FirebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
