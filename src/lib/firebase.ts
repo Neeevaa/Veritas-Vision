45a7dd53a866617270f01a0131707e9b7a53acaa
neeevaa codes <neeevaacodes@gmail.com>
2025-09-20 17:00:43 +0000

    i'm getting errors while signingin and signup normally as well as using

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 // to ensure it only runs when called (on the client-side).
 export function getFirebaseApp() {
     const firebaseConfig = {
-        apiKey: "AIzaSyBauvOVHec37k2Lw3NuM8KF1GHPx1hKC60",
-        authDomain: "studio-2631480391-e838b.firebaseapp.com",
-        projectId: "studio-2631480391-e838b",
-        storageBucket: "studio-2631480391-e838b.firebasestorage.app",
-        messagingSenderId: "984818009071",
-        appId: "1:984818009071:web:f30c0731ba56ad72fd6c1a"
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
       };
 
     if (!getApps().length) {
